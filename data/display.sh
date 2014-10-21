e7cdc4de2c64bc16a93e2ee7eff2d07702a586f2
lyanchih <lyanchih@Hungs-MacBook-Air.local>
2014-10-21 15:12:47 +0800

    Add simple shell file which will echo speed csv data.

---
+++ data/display.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+while getopts ":d:s:o:" flag
+do
+    case $flag in
+        d)
+            FOLDER=$OPTARG
+            ;;
+        s)
+            SORT_COL=$OPTARG
+            ;;
+        o)
+            OUTFILE=$OPTARG
+            ;;
+        ?)
+            echo "Invalid option: -$OPTARG"
+            exit -1
+            ;;
+    esac
+done
+
+if [[ -n "$SORT_COL" ]]; then
+    cat ${FILDER:-data}/*.csv | grep -v freeway_id | sort -t, -nk ${SORT_COL}
+else
+    cat ${FILDER:-data}/*.csv | grep -v freeway_id
+fi
+
+
+
+
+
